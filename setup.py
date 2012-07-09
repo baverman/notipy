bdfe7773f9aee5e4cf167848a5784d5b12d3f002
Vinycius Maia <suportevg@uol.com.br>
2012-07-09 07:52:13 -0300

    Update install script

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import distutils.core
+from distutils.core import setup
 
-distutils.core.setup(
+setup(
     name="notipy-fork",
     version="0.3.0",
     description="A minimalistic gtk3 notification daemon written in python.",
     author="Vinycius Maia",
     author_email="suportevg@uol.com.br",
+    license="GPLv3",
     url="https://github.com/mrk3004/notipy/",
-    scripts=["notipy"]
+    scripts=["notipy"],
+    data_files=[("/etc", ["notipy.conf"])]
     )
